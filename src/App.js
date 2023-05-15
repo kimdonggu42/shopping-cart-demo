13884cda58a2d2a6c934f87c86070d6f3cab5a98
kimdonggu42 <kdonggoo44@gmail.com>
2023-05-15 10:51:21 +0900

    Firebase realtime database 연결

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import MainHeader from './components/Header/MainHeader';
 import Cart from './components/MyCart/Cart';
 import Products from './components/ItemList/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 
 const ItemArea = styled.div`
   display: flex;
@@ -13,6 +16,16 @@ const ItemArea = styled.div`
 
 function App() {
   const showCart = useSelector(state => state.uiReducer.cartIsVisible);
+  const cart = useSelector(state => state.cartReducer);
+
+  console.log(cart)
+
+  useEffect(() => {
+    const putCartItem = async () => {
+      await axios.put(`https://redux-http-97631-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json`, cart)
+    }
+    putCartItem();
+  }, [cart])
 
   return (
     <>
